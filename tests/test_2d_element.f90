c70137e4f1ca5341d75e14d228558c77ec7b0937
Michael Olaya <michael.n.olaya@gmail.com>
2025-03-13 14:54:45 -0400

    write output to file instead of shell
    Signed-off-by: Michael Olaya <michael.n.olaya@gmail.com>

--- tests/test_2d_element.f90
+++ tests/test_2d_element.f90
@@ -8,7 +8,7 @@ program main
     implicit none
 
     integer, parameter :: ngrid = 50
-    integer :: i, j
+    integer :: i, j, funit
     real(r64) :: elem_coords(4, 2), eta_1(ngrid), eta_2(ngrid), grid(2, ngrid, ngrid), xy(2)
     real(r64), allocatable :: N(:, :), node_coords(:)
     type(LinearElement_t) :: elem
@@ -25,9 +25,12 @@ program main
     eta_2 = linspace(-1.0_r64, 1.0_r64, ngrid)
     grid = meshgrid(eta_1, eta_2)
 
+    ! Set file output
+    open(newunit=funit, file='element_results.txt')
+    
     ! Compute global coordinates and shape functions for verification
     node_coords = elem%get_nodal_coordinate_vec(loc='global')
-    print *, 'eta1 ', 'eta2 ', 'x1 ', 'x2 ', 'N1 ', 'N2 ', 'N3 ', 'N4'
+    write(funit, '(*(G0.15,:,","))') 'eta1', 'eta2', 'x1', 'x2', 'N1', 'N2', 'N3', 'N4'
     do i = 1, size(grid, dim=2)
         do j = 1, size(grid, dim=3)
             ! Shape function matrix 
@@ -35,7 +38,8 @@ program main
 
             ! Global xy coordinates
             xy = matmul(N, node_coords)
-            print *, grid(1, i, j), grid(2, i, j), xy, N(1, 1), N(1, 3), N(1, 5), N(1, 7)
+            write(funit, '(*(G0.15,:,","))') grid(1, i, j), grid(2, i, j), xy, N(1, 1), N(1, 3), N(1, 5), N(1, 7)
         end do
     end do
+    close(funit)
 end program main
